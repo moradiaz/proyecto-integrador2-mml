67aa611e1abaf6973259eaabf0f7e0bc9a50beda
luciana <lzeitune@udesa.edu.ar>
2023-09-04 10:09:19 -0300

    header y footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+export default function Footer() {
+  return (
+    <footer>
+        Morena Baron
+        Mora Diaz
+        Luciana Zeitune
+    </footer>
+  )
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './styles.css'
+
+const elementos = [
+    {
+        nombre: 'Home',
+        link:'/'
+    },
+    {
+        nombre: 'Favoritos',
+        link:'/favoritos'
+    },
+    {
+        nombre: 'Películas',
+        link: '/peliculas'
+    },
+    {
+        nombre: 'Series',
+        link: '/series'
+    }
+]
+export default function Header() {
+  return (
+    <nav>
+        <ul className='main-nav'>
+            {
+                elementos.map((elm) => <li>
+                    <Link to = {elm.link}>
+                        {elm.nombre}
+                    </Link>
+                    </li>)
+            }
+        </ul>
+        <ul>
+            <img className='logo' src= './img/logo.jpg'alt="" />
+        </ul>
+        
+    </nav>
+  )
+}
